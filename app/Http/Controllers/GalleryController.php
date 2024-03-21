acecf5e095fdbe8f68e44d8c7b326165a82b5753
carlosaw <carlosfreevg@gmail.com>
2024-03-20 21:18:37 -0400

    files

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -6,16 +6,35 @@ use Illuminate\Http\Request;
 
 class GalleryController extends Controller
 {
-    //
-    public function index() {
-        return view('index');
-    }
+  //
+  public function index()
+  {
+    return view('index');
+  }
+  public function upload(Request $request)
+  {
+    //$title = $request->only('title');
+    //dd($title);
 
-    public function upload(Request $request) {
-        dd($request);
-    }
-    public function delete() {
+    //dd($request->hasFile('image'));
+    if($request->hasFile('image')) {
+        $image = $request->file('image');
+        //$name = time() . '.' . rand(0,9999) . $image->getClientOriginalExtension();
+        //$name = time() . '.' . rand(0,9999) . $image->getClientOriginalName();
+        $name = $image->hashName();
+        //dd($name);
+        $return = $image->storePublicly('uploads', 'public', $name);
+        dd($return);
 
-    }
 
+        // $title = $request->input('title');
+        // $url = $request->input('url');
+    }
+    //$image= $request->file('image');
+    //dd($image);
+
+  }
+  public function delete()
+  {
+  }
 }
