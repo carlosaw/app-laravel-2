487751c630d676c9ccd3c812104c34c6c338cc6a
carlosaw <carlosfreevg@gmail.com>
2024-03-20 20:06:45 -0400

    correct delete

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [GalleryController::class, 'index'])->name('index');
 Route::post('/upload', [GalleryController::class, 'upload'])->name('upload');
-Route::get('/delte/{id}', [GalleryController::class, 'delete'])->name('delete');
+Route::get('/delete/{id}', [GalleryController::class, 'delete'])->name('delete');
